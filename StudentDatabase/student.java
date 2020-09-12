6db59a2f07d28a38aab7c770d31ff2b4620bb169
mangjoe2 <mariejoseph144@gmail.com>
2020-09-12 06:09:46 -0400

    My second Commit

--- StudentDatabase/student.java
+++ StudentDatabase/student.java
@@ -1,3 +1,76 @@
+package StudentDatabase;
+
+import java.util.Scanner;
+
 public class student {
+
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private String courses = "";
+    private int tuitionBalance;
+    private String studentID;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
+
+    //Constructor: prompt user to enter student name and year
+    public student(){
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("1 - Freshmen\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.gradeYear = in.nextLine();
+
+        setStudentID();
+        System.out.println(firstName +" " + lastName +" " + gradeYear +" " + studentID);
+
+    }
+    //Generate ID
+    private void setStudentID(){
+        //Grade level + ID
+        id++;
+        
+        this.studentID = gradeYear + "" + id;
+    }
+    //Enrol in courses
+    public void enroll(){
+        do{
+            System.out.println("Enter course to enroll(Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")){
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance +costOfCourse;
+            }else {
+                System.out.println("BREAK");
+                break;}
+        }while(1 != 0);
+        System.out.println("Enrolled In: " + courses);
+    }
+    //View balance
+    public void viewBalance(){
+        System.out.println("Your balance is: $"+tuitionBalance);
+    }
+    //Pay tuition
+    public void payTuition(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your payment: &");
+        int payment = in.nextInt();
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Thank you for your payment of $"+payment);
+        viewBalance();
+    }
+    //Show status
+    public String showInfo(){
+
+        return "Name: " + firstName+ " " + lastName +
+        "\nCourses Enrolled: "+ courses + 
+        "\nBalance: $" + tuitionBalance;
+    }
     
 }
--- StudentDatabase/studentDataApp.java
+++ StudentDatabase/studentDataApp.java
@@ -1,1 +1,50 @@
+package StudentDatabase;
 
+import java.util.Scanner;
+
+/**
+ * Project 2
+Student Database Application
+Scenario: You are a Database Administrator for a university and need to
+create an application to manage student enrollments and balance.
+
+Your application should do the following:
+
+Ask the user how many new students will be added to the database
+The user should be prompted to enter the name and year for each student
+The student should have a 5-digit unique ID, with the first number being their grade level
+A student can enroll in the following courses:
+History 101
+Mathematics 101
+English 101
+Chemistry 101
+Computer Science 101
+Each course costs $600 to enroll
+The student should be able to view their balance and pay the tuition
+To see the status of the student, we should see their name, ID, courses enrolled, and balance
+ */
+public class studentDataApp{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // student stu1 = new student();
+        
+        // stu1.enroll();
+        // stu1.payTuition();
+        // System.out.println(stu1.showInfo());
+
+        //Ask for how many new users we want to add
+        System.out.println("Enter the number of new students to enroll: ");
+        int numberOfStudent = in.nextInt();
+        student[] student = new student[numberOfStudent];
+        //Create n number of new students
+        for(int i=0; i<numberOfStudent; i++){
+            student[i] = new student();
+            student[i].enroll();
+            student[i].payTuition();
+        }
+        for(int i=0; i<numberOfStudent; i++){
+            System.out.println(student[0].showInfo());
+        }
+    }
+    
+}
